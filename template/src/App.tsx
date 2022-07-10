41c4bc7ba6c6e1dc6a4d51761afd6c8e0a8bfa76
abhilashlr <abhilashlr.789@gmail.com>
2022-07-10 13:02:11 +0530

    fix: move to styled and fix eslint

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,24 +1,61 @@
 import { FC } from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
-import './App.css';
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Logo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    .App-logo {
+      animation: App-logo-spin infinite 20s linear;
+    }
+  }
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const Link = styled.a`
+  color: #61dafb;
+`;
 
 export const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img alt="logo" className="App-logo" src={logo} />
+    <AppWrapper>
+      <AppHeader>
+        <Logo alt="logo" src={logo} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <Link
           href="https://reactjs.org"
           rel="noopener noreferrer"
           target="_blank"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </Link>
+      </AppHeader>
+    </AppWrapper>
   );
 };
